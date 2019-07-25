560ae02c56c0faf8b5f838b9a0dc9d5a72918530
VBTX <jv.svedaite@gmail.com>
2019-07-25 11:38:54 -0500

    add presentational MyQuotes component

---
+++ src/components/MyQuotes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+
+const MyQuotes = props => {
+	const quoteCards = props.quotes.length > 0 ? 
+	props.quotes.map( q => <Link to={`/quotes/${q.id}`} key={q.id}>{q.author}<br/></Link>) : "No quotes at the moment"
+	return quoteCards
+	}
+
+
+const mapStateToProps = state => {
+	return {
+	quotes: state.myQuotes
+}
+}
+
+export default connect(mapStateToProps)(MyQuotes)
