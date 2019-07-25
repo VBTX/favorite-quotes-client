15b36dc29548db14ef6b744731094c183825c3a4
VBTX <jv.svedaite@gmail.com>
2019-07-25 10:02:03 -0500

    add SET_MY_QUOTES

---
+++ src/reducers/myQuotes.js
@@ -0,0 +1,18 @@
+export default (state = [], action) => {
+	switch (action.type) {
+
+		case "SET_MY_QUOTES":
+		  return action.quotes
+		// case "ADD_QUOTE":
+		// return state.concat(action.quote)
+		// case "UPDATE_QUOTE":
+		// return state.map(quote => quote.id===action.quote.id ? action.quote : quote)
+		// case "DELETE_QUOTE_SUCCESS":
+		// // console.log(state.quotes, action.quoteId)
+		// return state.filter(quote => quote.id === action.quoteId ? false:true)
+		// case "CLEAR_QUOTES":
+		//   return []
+		default:
+		  return state
+	}
+}
