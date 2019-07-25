eeb6159e340b9db9673c89a257a4b50fb47ddade
VBTX <jv.svedaite@gmail.com>
2019-07-25 12:05:13 -0500

    add some style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import ReactDOM from 'react-dom';
+
+
 import App from './App';
 import store from './store.js'
 import { Provider } from 'react-redux'
