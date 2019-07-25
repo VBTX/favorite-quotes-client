b354ca975fd041023709c5dcff643a4c79a78312
VBTX <jv.svedaite@gmail.com>
2019-07-25 11:15:42 -0500

    import Router and wrap App in Router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Login from "./components/LoginForm"
 import Home from "./components/Home"
 import { getCurrentUser } from "./actions/currentUser"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar"
 
 class App extends Component {
 
@@ -16,9 +18,11 @@ class App extends Component {
   const {loggedIn, allQuotes} = this.props
   return (
     <div className="App">
-        
+    <Router>
+    {loggedIn ? <NavBar /> : null }   
     <h1>App component</h1>
     <Login />
+    </Router>
     </div>
 
   );
