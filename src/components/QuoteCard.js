1b6686e99116868f65422e6a641aa711bb8d21df
VBTX <jv.svedaite@gmail.com>
2019-07-25 12:28:32 -0500

    add link to edit

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -12,7 +12,7 @@ const QuoteCard = ({quote}) => {
 		<h2>{quote.author}</h2><br />
 		<h3>{quote.text}</h3> <br/>
 		<h4>{quote.source}</h4><br/>
-		<Link to={`/quotes/${quote.id}/edit`}>Edit Trip Info</Link></div> :
+		<Link to={`/quotes/${quote.id}/edit`}>Edit Quote</Link></div> :
 		null
 		)
 }
