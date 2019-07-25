53e63b7eee163c78c03974d3951efa3e0ba51158
VBTX <jv.svedaite@gmail.com>
2019-07-25 12:29:19 -0500

    add routes to specific quotes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import './App.css';
-import SignUp from "./components/Signup"
+import Signup from "./components/Signup"
 import Login from "./components/LoginForm"
 import Home from "./components/Home"
 import MyQuotes from "./components/MyQuotes"
+import QuoteCard from "./components/QuoteCard"
 import { getCurrentUser } from "./actions/currentUser"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar"
@@ -24,9 +25,17 @@ class App extends Component {
     <Router>
     {loggedIn ? <NavBar /> : <h3>Welcome! Please <Link to="/signup" className="link">Sign Up</Link> or <Link to="/login" className="link">Log In</Link>!</h3> }   
     <h1>App component</h1>
+    <Switch>
     <Route exact path="/login" component={Login}/>
+    <Route exact path="/signup" component={Signup}/>
     <Route exact path="/my-quotes" component={MyQuotes}/>
+    <Route exact path="/my-quotes/:id"    render={props => {
+           const quote = allQuotes.find((quote) => quote.id === parseInt(props.match.params.id));
+            return <QuoteCard quote={quote} {...props}/>
+            }}/>
+          </Switch>
     </Router>
+
     </div>
 
   );
@@ -46,3 +55,7 @@ export default connect(mapStateToProps, { getCurrentUser })(App)
 
 
  // { loggedIn ? <Home /> : <Login /> }
+ // <Route exact path="/quotes/:id/edit"    render={props => {
+ //           const quote = allquotes.find((quote) => quote.id === parseInt(props.match.params.id));
+ //            return <EditFormWrapper quote={quote} {...props}/>
+ //            }}/>
