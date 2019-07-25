7ca22ca62da59e6102a04dd16cba6e0a4af26dab
VBTX <jv.svedaite@gmail.com>
2019-07-25 12:27:50 -0500

    add my quotes reducers

--- src/reducers/myQuotes.js
+++ src/reducers/myQuotes.js
@@ -5,8 +5,8 @@ export default (state = [], action) => {
 		  return action.quotes
 		// case "ADD_QUOTE":
 		// return state.concat(action.quote)
-		// case "UPDATE_QUOTE":
-		// return state.map(quote => quote.id===action.quote.id ? action.quote : quote)
+		case "UPDATE_QUOTE":
+		return state.map(quote => quote.id===action.quote.id ? action.quote : quote)
 		// case "DELETE_QUOTE_SUCCESS":
 		// // console.log(state.quotes, action.quoteId)
 		// return state.filter(quote => quote.id === action.quoteId ? false:true)
