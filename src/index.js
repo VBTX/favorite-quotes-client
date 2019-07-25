b81e0c854a27bb85bf182e1b17a9fceda77a3e13
VBTX <jv.svedaite@gmail.com>
2019-07-25 13:46:42 -0500

    remove unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import ReactDOM from 'react-dom';
 
 
