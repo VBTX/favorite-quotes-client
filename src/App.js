76eab3b2f66000fb406a6ba3e83640fd26703fa4
VBTX <jv.svedaite@gmail.com>
2019-07-25 11:30:32 -0500

    import signup and link to add links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import './App.css';
+import SignUp from "./components/Signup"
 import Login from "./components/LoginForm"
 import Home from "./components/Home"
 import { getCurrentUser } from "./actions/currentUser"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar"
+import { Link } from "react-router-dom";
 
 class App extends Component {
 
@@ -19,9 +21,9 @@ class App extends Component {
   return (
     <div className="App">
     <Router>
-    {loggedIn ? <NavBar /> : null }   
+    {loggedIn ? <NavBar /> : <h3>Welcome! Please <Link to="/signup" className="link">Sign Up</Link> or <Link to="/login" className="link">Log In</Link>!</h3> }   
     <h1>App component</h1>
-    <Login />
+    <Route exact path="/login" component={Login}/>
     </Router>
     </div>
 
