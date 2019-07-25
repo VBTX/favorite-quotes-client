1b36d8a09759ccf3379875aa381b06ab9f12ba9a
VBTX <jv.svedaite@gmail.com>
2019-07-25 12:52:41 -0500

    handleSubmit for the existing quote

---
+++ src/components/EditQuoteForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { setFormDataForEdit } from "../actions/quoteForm"
+import { updateQuote, deleteQuote } from "../actions/myQuotes"
+import { connect } from "react-redux"
+import QuoteForm from "./QuoteForm"
+import { resetQuoteForm } from "../actions/quoteForm"
+
+
+
+class EditFormWrapper extends Component {
+ 
+
+ componentDidMount(){
+ 	this.props.quote && this.props.setFormDataForEdit(this.props.quote)
+ }
+
+ componentDidUpdate(prevProps){
+ 	this.props.quote && !prevProps.quote && this.props.setFormDataForEdit(this.props.quote)
+
+ }
+
+ componentWillUnmount(){
+ 		this.props.resetQuoteForm()
+ }
+ handleSubmit = (event, formData, userId) => {
+ 		const { updateQuote, quote, history } = this.props
+		event.preventDefault()
+		updateQuote({
+            ...formData,
+            quoteId: quote.id,
+            userId
+        }, history)
+	}
+
+	render() {
+		const { history, deleteQuote, quote } = this.props
+		const quoteId = quote ? quote.id : null 
+	return <>
+			<QuoteForm editMode history={this.history} handleSubmit={this.handleSubmit}/>
+			<button style={{color:"red"}} onClick={()=>deleteQuote(quoteId, history)}> Delete </button>
+			</>
+}
+	};
+
+
+export default connect(null, { updateQuote, setFormDataForEdit, resetNewForm, deletequote })(EditQuoteForm);
