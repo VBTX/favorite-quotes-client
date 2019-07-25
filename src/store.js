2b4ccb00eb790659a4b86397ae1969ebabf31768
VBTX <jv.svedaite@gmail.com>
2019-07-25 12:27:16 -0500

    add quote form to the store

--- src/store.js
+++ src/store.js
@@ -4,13 +4,15 @@ import currentUser from './reducers/currentUser'
 import loginForm from './reducers/loginForm'
 import myQuotes from './reducers/myQuotes'
 import signupForm from './reducers/signupForm'
+import quoteForm from './reducers/quoteForm'
 
 
 const reducer = combineReducers({
 	currentUser,
 	loginForm,
 	myQuotes,
-	signupForm
+	signupForm,
+	quoteForm
 	})
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
