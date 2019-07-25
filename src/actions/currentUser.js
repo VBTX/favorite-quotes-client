a99442a31fc140d70e18e727ad0cc759a75e3bcb
VBTX <jv.svedaite@gmail.com>
2019-07-25 10:14:08 -0500

    add getCurrentUser action creator

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -22,6 +22,29 @@ export const setCurrentUser = user => {
 
 //ASYNC
 
+export const getCurrentUser = () => {
+  return dispatch =>{
+    return fetch("http://localhost:3001/api/v1/get_current_user",{
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    .then(r => r.json())
+    .then(user => {
+      if (user.error) {
+        alert(user.error)
+      } else {
+        dispatch(setCurrentUser(user))
+        dispatch(getMyQuotes())
+      }
+
+
+      })
+    .catch(console.log)
+  }
+ }
 
 export const login = (credentials, history) => {
  	return dispatch =>{
