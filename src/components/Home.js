8dbbc65d0841eb4056e6a7b3afc717e4b237fa29
VBTX <jv.svedaite@gmail.com>
2019-07-25 11:04:21 -0500

    add welcome message and links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
 // import Login from './LoginForm'
 // import Signup from './SignUp'
+import Logout from './Logout'
 import { connect } from 'react-redux'
 import { Link } from "react-router-dom";
 import '../App.css';
 
 
-const Home = ({currentUser}) => (
+const Home = ({currentUser, loggedIn}) => (
 	<div>
-	<h3>Welcome! Please <Link to="/signup" className="link">Sign Up</Link> or <Link to="/login" className="link">Log In</Link>!</h3>
+	<p> Home Component </p>
+	{ loggedIn ? <Logout/> : <h3>Welcome! Please <Link to="/signup" className="link">Sign Up</Link> or <Link to="/login" className="link">Log In</Link>!</h3>} 	
+		
 	</div>
 );
 
