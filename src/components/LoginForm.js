ce3a5ea213d0f5339a7c0d25f80992f3b6ffbf1b
VBTX <jv.svedaite@gmail.com>
2019-07-25 08:41:55 -0500

    add handleInputChange, handleSubmit and passed props from store plus actions through redux

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,47 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { login } from '../actions/currentUser'
+import { updateLoginForm } from '../actions/loginForm'
 
-const Login = ({}) => {
+const Login = ({ loginForm, updateLoginForm, login, history }) => {
+	const handleInputChange = event =>  {
+		const { name, value } = event.target
+		const updatedFormInfo = {
+			...loginForm,
+			[name]:value
 
+		}
+		updateLoginForm(updatedFormInfo)
+		//passing the object into a function that came from imported action
+	
+	}
 
+	const handleSubmit = event => {
+		event.preventDefault()
+		login(loginForm, history)
 
+}
 
+return (
+		<form onSubmit={handleSubmit}>
+		<input 
+		type="text"
+		name="email"
+		value={loginForm.email}
+		placeholder="email"
+		onChange={handleInputChange}
+		/>
+		<input 
+		type="password"
+		name="password"
+		value={loginForm.password}
+		placeholder="password"
+		onChange={handleInputChange}
+		/>
+		<input type="submit" value="Log In"/>
+		</form>
 
-
-
-
-
+		)
 }
 
 const mapStateToProps = state => {
