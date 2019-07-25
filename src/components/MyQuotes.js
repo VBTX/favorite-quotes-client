0b19edcb8cb65a80ac6348a785486eaf01e97c26
VBTX <jv.svedaite@gmail.com>
2019-07-25 12:31:40 -0500

    map through quotes

--- src/components/MyQuotes.js
+++ src/components/MyQuotes.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const MyQuotes = props => {
 	const quoteCards = props.quotes.length > 0 ? 
-	props.quotes.map( q => <Link to={`/quotes/${q.id}`} key={q.id}>{q.author}<br/></Link>) : "No quotes at the moment"
+	props.quotes.map( q => <Link to={`/my-quotes/${q.id}`} key={q.id}>{q.author}<br/></Link>) : "No quotes at the moment"
 	return quoteCards
 	}
 
