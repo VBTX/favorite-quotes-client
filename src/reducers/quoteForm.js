649a69c5ae2369a1b6cd3be4274e5b7307306bbb
VBTX <jv.svedaite@gmail.com>
2019-07-25 12:26:41 -0500

    add quote form reducers

---
+++ src/reducers/quoteForm.js
@@ -0,0 +1,21 @@
+const initialState = {
+	author: "",
+	text: "",
+	source: ""
+}
+
+export default (state = initialState, action) => {
+	switch (action.type) {
+		case "UPDATE_QUOTE_FORM":
+		return action.formData
+
+		case "RESET_QUOTE_FORM":
+		return initialState
+
+		case "SET_FORM_DATA_FOR_EDIT":
+		return action.quoteData
+		
+		default:
+		  return state
+	}
+}
