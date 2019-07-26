daa99812b3a86905a8eb125cece6e9378b3d07f4
VBTX <jv.svedaite@gmail.com>
2019-07-26 08:40:23 -0500

    temp hide see notes button

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -23,11 +23,7 @@ const QuoteCard = ({quote}) => {
 		<Card.Header><b>Author: {quote.author} </b></Card.Header><br />
 		<Card.Body>{quote.text}<br/><br/>
 		<h6>Source: {quote.source}</h6>
-		<br/>
-		 { quote.notes.length > 0 ? 
-            <Button variant="outline-secondary" onClick={handleSubmit}>See Notes</Button> : 
-            <Button type="submit" variant="outline-secondary">Add Note</Button> 
-        }
+		
 		<Button variant="outline-secondary"><Link to={`/my-quotes/${quote.id}/edit`}>Edit Quote</Link></Button></Card.Body></Card> :
 		null
 		)
@@ -36,3 +32,9 @@ const QuoteCard = ({quote}) => {
 export default QuoteCard
 
 		// <div className="quoteCard">
+
+		// <br/>
+		//  { quote.notes.length > 0 ? 
+  //           <Button variant="outline-secondary" onClick={handleSubmit}>See Notes</Button> : 
+  //           <Button type="submit" variant="outline-secondary">Add Note</Button> 
+  //       }
