811d751da71663c72712186995e2fa64a5b1a78b
VBTX <jv.svedaite@gmail.com>
2019-07-28 16:42:39 -0500

    add search actions

---
+++ src/actions/search.js
@@ -0,0 +1,18 @@
+export const setSearch = search => {
+	return {
+		type: "SET_SEARCH",
+		search
+	}
+}
+
+
+export const findIt = (search) => {
+	return {
+		type: "FIND_IT",
+		search
+	}
+}
+
+
+//we will dispatch this object
+
