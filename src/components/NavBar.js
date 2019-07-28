be143f6bbfb85b1097a24a8dcb4ea05052982a89
VBTX <jv.svedaite@gmail.com>
2019-07-28 17:47:03 -0500

    add search link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,8 +15,9 @@ const NavBar = ({setSearch, quotes}) => {
 	return (
 		<div className="nav">
 		<NavLink exact className="activeClass" to="/">Home  </NavLink>
-		<NavLink exact className="activeClass" to="/my-quotes">My Quotes  </NavLink>
+		<NavLink exact className="activeClass" to="/my-quotes">My Quotes </NavLink>
 		<NavLink exact className="activeClass" to="/my-quotes/new">New Quote  </NavLink>
+		<NavLink exact className="activeClass" to="/search"> Search Quotes </NavLink>
 		<Logout />
 		</div>
 		)
