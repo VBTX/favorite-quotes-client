558d1c20ff32ccac8d7e7da85892a7a42cd07e7a
VBTX <jv.svedaite@gmail.com>
2019-07-29 07:50:21 -0500

    add styling

--- src/components/QuickQuoteCard.js
+++ src/components/QuickQuoteCard.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './QuickCard.css'
 import { Link } from 'react-router-dom'
+// import ListGroup from 'react-bootstrap/ListGroup'
+
 
 
 
@@ -11,7 +13,7 @@ const QuickQuoteCard = ({quote}) => {
 		quote ?
 		<div className="QuickCard">
 		<h3><i><strong><Link to={`/my-quotes/${quote.id}`} className="link" key={quote.id}>{quote.text|| quote.quote}</Link></strong></i></h3><br/>
-		<h5>Author: <a href={`https://www.google.com/search?q=${quote.author}`} target="_blank">{quote.author}</a>; Source: {quote.source || <a href="theysaidso.com">TheySaidSo</a>}</h5></div> :
+		<h5>Author: <a href={`https://www.google.com/search?q=${quote.author}`} target="_blank">{quote.author}</a>; Source: {quote.source || <a href="theysaidso.com">TheySaidSo</a>}</h5></div>:
 		null
 		)
 }
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 // import NoteList from "./NoteList"
 import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 
 
@@ -15,13 +16,15 @@ const QuoteCard = ({quote}) => {
 	return (
 
 		quote ?
-		<Card border="success" style={{ width: '18rem', display: 'inline-block'}}>
+		<Card border="success" style={{ width: '40rem', display: 'inline-block', padding: '10px'}}>
 
-		<Card.Header><b>Author: {quote.author} </b></Card.Header><br />
-		<Card.Body>{quote.text}<br/><br/>
+		<Card.Header><b><h1>Author: {quote.author} </h1></b></Card.Header><br />
+		<Card.Body>
+		<ListGroup variant="flush">
+		<h2>{quote.text}</h2><br/><br/>
 		<h6>Source: {quote.source}</h6>
 		
-		<Button variant="outline-secondary"><Link to={`/my-quotes/${quote.id}/edit`}>Edit Quote</Link></Button></Card.Body></Card> :
+		<Button variant="outline-secondary"><Link to={`/my-quotes/${quote.id}/edit`}><h3>Edit Quote</h3></Link></Button></ListGroup></Card.Body></Card> :
 		null
 		)
 }
