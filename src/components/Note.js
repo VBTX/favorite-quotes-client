680c2d6987b81a7de80d5cae8e8085ea6bd204ae
VBTX <jv.svedaite@gmail.com>
2019-07-29 16:08:11 -0500

    create Note component

---
+++ src/components/Note.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { createNote } from "../actions/myNotes"
+import { connect } from "react-redux"
+import { withRouter } from "react-router-dom"
+
+class Note extends React.Component {
+
+  state={
+    text: ""
+  }
+  handleChange = event => {
+    const {name, value} = event.target
+    this.setState({
+      [name]:value
+    })
+  }
+  	
+
+  handleSubmit = (event, userId, quoteId, history) => {
+  	event.preventDefault()
+  	this.props.createNote({text: this.state.text, userId:this.props.userId, quoteId:this.props.quoteId}, this.props.history)
+    };
+
+  render() {
+    return (
+      <div>
+      	<form onSubmit={this.handleSubmit}>
+      	<h3>Add a note:</h3>
+      	<textarea type="text" name="text" value={this.props.text}
+      	onChange={this.handleChange} /><br/>
+      	<Button type="submit" variant="secondary">SUBMIT</Button>
+      	</form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    userId: state.currentUser.id
+  }
+}
+
+export default withRouter(connect(mapStateToProps, { createNote })(Note));
+
+
+// <ul>
+//           {this.state.comments.map((c,i) => <li key={i}> {c} </li>)}
+
+//         </ul>    
+
+
+// this.setState({ 
+//       text: event.target.value
+//     })
+//   }
+
+
+
+    // reduxaction(us)
+  //  this.setState({
+  //    comments:[...this.state.comments, comment],
+  //    text:""
+  //  })
+
+  // }
