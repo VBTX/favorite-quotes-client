1944797f5848a44bd9f8aafff7e26a13d435265b
VBTX <jv.svedaite@gmail.com>
2019-07-29 16:58:16 -0500

    add update Note and delete Note async actions

--- src/actions/myNotes.js
+++ src/actions/myNotes.js
@@ -22,9 +22,63 @@ export const createNote = (noteData, history) => {
 			if (note.error) {
 				alert(note.error)
 			} else {
+			history.push("/my-quotes/")
+		}
+		})
+		.catch(console.log)
+	}
+}
+
+export const updateNote = (noteData, history) => {
+
+	return dispatch => {
+		const sendableNoteData = {
+			note: {
+				text: noteData.text,
+				quote_id: noteData.quoteId,
+				user_id: noteData.userId
+			}
+		}
+		return fetch(`http://localhost:3001/api/v1/notes/${noteData.id}`, {
+			credentials: "include",
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(sendableNoteData)
+		})
+		.then(r=> r.json())
+		.then(quote => {
+			if (quote.error) {
+				alert(quote.error)
+			} else{
+			history.push(`history.location.pathname`)
+		}
+		})
+		.catch(console.log)
+	}
+}
+
+export const deleteNote = (note, history) => {
+	return dispatch => {
+		return fetch(`http://localhost:3001/api/v1/notes/${note.id}`, {
+			credentials: "include",
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+		.then(r=> r.json())
+		.then(note=> {
+			if (note.error) {
+				alert(note.error)
+			} else{
+			const path = history.location.pathname
 			history.push("/my-quotes")
 		}
 		})
 		.catch(console.log)
 	}
 }
+
+
