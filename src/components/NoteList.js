0cf287707b05034db97d49f1df362466348cc0b0
VBTX <jv.svedaite@gmail.com>
2019-07-29 16:58:37 -0500

    add delete button

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,11 +1,19 @@
 
 import React from 'react'
+import { connect } from 'react-redux'
+import Button from 'react-bootstrap/Button';
+import { deleteNote } from '../actions/myNotes'
 
-const NoteList = ({notes}) => {
+const NoteList = ({notes, deleteNote, history }) => {
   return (
-    <div><ul>{notes.map((n, i) => <h3  key={i}><li>{n.text}</li></h3>)}</ul></div>
+    <div><ul>{notes.map((n, i) => <h3  key={i}><li>{n.text}<Button variant="outline-danger" onClick={()=>deleteNote(n, history)}> Delete </Button></li></h3>)}</ul></div>
   );
 };
 
 
-export default NoteList
+
+
+
+
+
+export default connect(null, { deleteNote })(NoteList)
