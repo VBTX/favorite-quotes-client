4f976aa511be4e47c35c2f5f291d5397e3b0077c
VBTX <jv.svedaite@gmail.com>
2019-08-01 15:44:02 -0500

    add note edit component

---
+++ src/components/NoteEditForm.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { updateNote } from "../actions/myNotes"
+import { connect } from "react-redux"
+import Button from 'react-bootstrap/Button';
+
+
+
+
+class NoteEditForm extends Component {
+
+
+render() {
+     return <div>
+      	<form onSubmit={this.handleSubmit}>
+      	<textarea type="text" name="text" value={console.log(this.props.note.text)}
+      	onChange={this.handleChange} /><Button type="submit" variant="secondary">SUBMIT</Button>
+      	</form>
+      </div>
+
+  }
+}
+
+
+
+
+
+
+export default connect(null, { updateNote })(NoteEditForm);
+
+
+     
+
+
+
+//  componentDidMount(){
+//  	this.props.quote && this.props.setFormDataForEdit(this.props.quote)
+//  }
+
+//  componentDidUpdate(prevProps){
+//  	this.props.quote && !prevProps.quote && this.props.setFormDataForEdit(this.props.quote)
+
+//  }
+
+//  componentWillUnmount(){
+//  		this.props.resetQuoteForm()
+//  }
+//  handleSubmit = (event, formData, userId) => {
+//  		const { updateQuote, quote, history } = this.props
+// 		event.preventDefault()
+// 		updateQuote({
+//             ...formData,
+//             quoteId: quote.id,
+//             userId
+//         }, history)
+// 	}
+
+// 	render() {
+
+// 	return <>
+// 			<QuoteForm editMode history={this.history} handleSubmit={this.handleSubmit}/>
+// 			</>
+// }
+// 	};
