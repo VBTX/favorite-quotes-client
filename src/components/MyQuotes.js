10f8171d20872c2d312c89c2a76b7de54dbd6abd
VBTX <jv.svedaite@gmail.com>
2019-08-02 06:55:52 -0500

    change styling

--- src/components/MyQuotes.js
+++ src/components/MyQuotes.js
@@ -5,7 +5,7 @@ import QuickQuoteCard from './QuickQuoteCard'
 
 const MyQuotes = props => {
 	const quoteCards = props.quotes.length > 0 ? 
-	<div align="center">{props.quotes.map( q => <QuickQuoteCard key={q.id} quote={q}/>)}</div> : "No quotes at the moment"
+	<div className="myQuotes">{props.quotes.map( q => <QuickQuoteCard key={q.id} quote={q}/>)}</div> : "No quotes at the moment"
 	return quoteCards
 	}
 
