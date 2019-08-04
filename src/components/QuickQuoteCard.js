8e6b8780b806e4e18f84dcd54cd55ac434455380
VBTX <jv.svedaite@gmail.com>
2019-08-03 19:18:09 -0500

    remove sample flip option

--- src/components/QuickQuoteCard.js
+++ src/components/QuickQuoteCard.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './QuickCard.css'
 import { Link } from 'react-router-dom'
-import Vote from './Vote'
 import { createQuote } from "../actions/myQuotes"
 import { connect } from "react-redux"
 import Button from 'react-bootstrap/Button';
@@ -13,6 +12,8 @@ import { withRouter } from "react-router-dom"
 
 const QuickQuoteCard = ({quote, createQuote, userId, history}) => {
 
+
+
 const handleSubmit = (event) => {
 
 		event.preventDefault()
@@ -22,17 +23,14 @@ const handleSubmit = (event) => {
         }, history)
 }
 
-	
+
+
 	return (
 
-		quote ?
 		<div className="QuickCard">
 		<h3><i><strong><Link to={`/my-quotes/${quote.id}`} className="link" key={quote.id}>{quote.text|| quote.quote}</Link></strong></i></h3><br/>
 		<h5>Author: <a href={`https://www.google.com/search?q=${quote.author}`} target="_blank" rel="noopener noreferrer">{quote.author}</a>; Source: {quote.source || <a href="theysaidso.com">TheySaidSo</a>}</h5>
-		{ quote.quote ? <Button type="submit" variant="success" onClick={(quote, history)=>{handleSubmit(quote, history)}}>Add to Favorites</Button> : null  }		
-
-		<Vote /></div>:
-		null
+		{ quote.quote ? <Button type="submit" variant="success" onClick={(event)=>{handleSubmit(event)}}>Add to Favorites</Button> : null  }</div>
 		)
 }
 
@@ -47,7 +45,7 @@ export default withRouter(connect(mapStateToProps, { createQuote })(QuickQuoteCa
 
 
 
-
-
-
-	
+	// handleToggle = event => {
+// 	console.log(event)
+// 	this.setState({toggled: !this.state.toggled})
+// }
