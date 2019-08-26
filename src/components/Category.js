ff0c25ba369ac93a54ed6d6ffb54239c6a1ca84e
VBTX <jv.svedaite@gmail.com>
2019-08-26 18:13:17 -0500

    add category

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,7 +1,73 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { createCategory } from "../actions/myNotes"
+import { connect } from "react-redux"
+import { withRouter } from "react-router-dom"
+import ListGroup from 'react-bootstrap/ListGroup'
 
-const Category = (props) => {
-  return (
-    <div></div>
-  );
-};
+class Note extends React.Component {
+
+  state={
+    text: ""
+  }
+  handleChange = event => {
+    const {name, value} = event.target
+    this.setState({
+      [name]:value
+    })
+  }
+  	
+
+  handleSubmit = (event, userId, quoteId, history) => {
+  	event.preventDefault()
+    // this.setState({...this.state.text, [this.state.submit]: true})
+  	this.props.createNote({text: this.state.text, userId:this.props.userId, quoteId:this.props.quoteId}, this.props.history)
+    this.mainInput.value = ""
+    };
+  
+
+  render() {
+    return (
+      <div>
+      	<form onSubmit={this.handleSubmit}>
+        <ListGroup variant="flush">
+      	<h3>Add a note:</h3>
+      	<textarea ref={(ref) => this.mainInput= ref} type="text" name="text" value={this.props.text}
+      	onChange={this.handleChange} /><br/>
+      	<Button type="submit" variant="secondary">SUBMIT</Button>
+         </ListGroup>
+      	</form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    userId: state.currentUser.id
+  }
+}
+
+export default withRouter(connect(mapStateToProps, { createNote })(Note));
+
+
+// <ul>
+//           {this.state.comments.map((c,i) => <li key={i}> {c} </li>)}
+
+//         </ul>    
+
+
+// this.setState({ 
+//       text: event.target.value
+//     })
+//   }
+
+
+
+    // reduxaction(us)
+  //  this.setState({
+  //    comments:[...this.state.comments, comment],
+  //    text:""
+  //  })
+
+  // }
--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -1,8 +1,22 @@
-export default (state = null, action) => {
+export default (state = [], action) => {
 	switch (action.type) {
-		case "SET_CATEGORY":
-			return action.category
+
+		case "SET_MY_CATEGORIES":
+		return action.categories
+		case "ADD_CATEGORY":
+		return state.concat(action.category)
+		case "UPDATE_CATEGORY":
+		return state.map(category => category.id===action.category.id ? action.category : category)
+		case "DELETE_CATEGORY_SUCCESS":
+		// console.log(state.quotes, action.quoteId)
+		return state.filter(category => category.id === action.categoryId ? false:true)
+		case "CLEAR_CATEGORIES":
+		  return []
+		case "FIND_IT":
+		return state.filter(quote => quote.text.includes(action.search))
+		// case "ADD_NOTE":
+		// return state.find(q => q.id===action.note.quote_id ? q.notes.push(action.note) :false)
 		default:
-		  	return state
+		  return state
 	}
 }
