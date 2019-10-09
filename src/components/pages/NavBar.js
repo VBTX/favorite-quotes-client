1990b40ee94bd7913f384daf3892be4e49289ae6
VBTX <jv.svedaite@gmail.com>
2019-10-08 19:46:30 -0500

    edit how navbar link for logout appears

--- src/components/pages/NavBar.js
+++ src/components/pages/NavBar.js
@@ -1,36 +1,35 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import Logout from '../login/Logout'
-// import Login from './LoginForm'
-import { NavLink } from 'react-router-dom'
-// import { setSearch } from '../actions/search'
+import React from 'react';
+import { connect } from 'react-redux';
+import Logout from '../login/Logout';
+import { NavLink } from 'react-router-dom';
 
-// import Navbar from 'react-bootstrap/Navbar'
-// import Nav from 'react-bootstrap/Nav'
-
-
-
-const NavBar = ({setSearch, quotes}) => {
-
+const NavBar = ({ setSearch, quotes }) => {
 	return (
-		<div className="nav">
-		<NavLink exact className="activeClass nav-link" to="/">Home  </NavLink>
-		<NavLink exact className="activeClass nav-link" to="/my-quotes">My Quotes </NavLink>
-		<NavLink exact className="activeClass nav-link" to="/my-quotes/new">New Quote  </NavLink>
-		<NavLink exact className="activeClass nav-link" to="/search"> Search Quotes </NavLink>
-		<Logout />
+		<div className='nav'>
+			<NavLink exact className='activeClass nav-link' to='/'>
+				Home{' '}
+			</NavLink>
+			<NavLink exact className='activeClass nav-link' to='/my-quotes'>
+				My Quotes{' '}
+			</NavLink>
+			<NavLink exact className='activeClass nav-link' to='/my-quotes/new'>
+				New Quote{' '}
+			</NavLink>
+			<NavLink exact className='activeClass nav-link' to='/search'>
+				{' '}
+				Search Quotes{' '}
+			</NavLink>
+			<div className='activeClass nav-link'>
+				<Logout />
+			</div>
 		</div>
-		)
-}
+	);
+};
 const mapStateToProps = state => {
-  return {
-    currentUser: state.currentUser,
-    loggedIn: !!state.currentUser,
-  }
-}
+	return {
+		currentUser: state.currentUser,
+		loggedIn: !!state.currentUser
+	};
+};
 
-
-export default connect(mapStateToProps)(NavBar)
-	// <NavLink exact activeClass to="/logout">Log Out</NavLink>
-
-// <div className="nav">
+export default connect(mapStateToProps)(NavBar);
