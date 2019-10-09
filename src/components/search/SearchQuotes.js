70c1b2e8195bc12af01cfc5428e2c3baf0b3ac45
VBTX <jv.svedaite@gmail.com>
2019-10-09 09:30:44 -0500

    fix misalignment between Quote Card and Note Card with display flex

--- src/components/search/SearchQuotes.js
+++ src/components/search/SearchQuotes.js
@@ -1,80 +1,80 @@
 import React, { Component } from 'react';
 import { setSearch, resetSearchForm } from '../../actions/search';
 import { connect } from 'react-redux';
-import QuoteCard from '../quotes/QuoteCard'
+import QuoteCard from '../quotes/QuoteCard';
 import Button from 'react-bootstrap/Button';
 // import NavBar from 'react-bootstrap/NavBar';
-import { withRouter } from "react-router-dom"
+import { withRouter } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 
-
-
 class SearchQuotes extends Component {
-
 	state = {
 		quote: {},
 		searched: false
-	}
+	};
 
+	handleInputChange = event => {
+		const { name, value } = event.target;
+		const input = {
+			[name]: value
+		};
+		this.props.setSearch(input);
+	};
 
-		handleInputChange = event =>  {
-			const { name, value } = event.target
-			const input = {
-				[name]:value
-
-			}
-			this.props.setSearch(input)
-	
+	handleSubmit = event => {
+		event.preventDefault();
+		const { quotes, search } = this.props;
+		const quote = quotes.filter(q => q.text.includes(search.search));
+		if (quote.length > 0) {
+			this.setState({ quote: quote, searched: true });
+		} else {
+			this.setState({ quote: quote, searched: true });
 		}
+		resetSearchForm();
+	};
 
-		handleSubmit = (event) => {
-		event.preventDefault()	
-		const { quotes, search } = this.props	
- 		const quote = quotes.filter(q => q.text.includes(search.search))
- 		if (quote.length>0){
- 			this.setState({quote:quote, searched: true})
- 		} else {
- 			this.setState({quote:quote, searched: true})
- 		}
- 		resetSearchForm()	
+	render() {
+		return (
+			<>
+				<Form className='mr-sm-2' onSubmit={this.handleSubmit}>
+					<h2>
+						<input
+							name='search'
+							placeholder='Search Your Quotes'
+							type='text'
+							onChange={this.handleInputChange}
+						/>
+					</h2>
+					<Button variant='success' type='submit'>
+						SEARCH
+					</Button>
+				</Form>
+				<br />
+				<div className='col-md-12' style={{ display: 'flex' }}>
+					{this.state.quote.length === 0 ? (
+						<h2>No quote matched your search</h2>
+					) : (
+						<QuoteCard quote={this.state.quote[0]} />
+					)}
+				</div>
+			</>
+		);
+	}
 }
-
-
-		
-
-
-
- render () {
- 				return (<><Form className="mr-sm-2" onSubmit={this.handleSubmit}>
-					<h2><input
-			        name="search"
-			        placeholder="Search Your Quotes"
-			        type="text"
-			        onChange={this.handleInputChange}
-			      /></h2><Button variant="success" type="submit">SEARCH</Button>
-			   </Form><br/>
-			        {(this.state.quote.length === 0) ? 
-			      	<h2>No quote matched your search</h2> :  <QuoteCard quote={this.state.quote[0]}/>
-			      }</>
- 			   )
- 			}
-}
-
-
 
 const mapStateToProps = state => {
-			return ({
-			search: state.search,
-		    quotes: state.myQuotes
-			})
-		}
+	return {
+		search: state.search,
+		quotes: state.myQuotes
+	};
+};
 
-
-		const mapDispatchToProps = {
-			setSearch
-		}
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SearchQuotes));
-
-
-
-
+const mapDispatchToProps = {
+	setSearch
+};
+export default withRouter(
+	connect(
+		mapStateToProps,
+		mapDispatchToProps
+	)(SearchQuotes)
+);
