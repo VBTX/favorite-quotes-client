8b91cba93490ab454e35e76eb6713b5ecb8ed4d4
VBTX <jv.svedaite@gmail.com>
2019-10-08 19:46:04 -0500

    edit style of the logout to match other navbar links

--- src/components/login/Logout.js
+++ src/components/login/Logout.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { logout } from '../../actions/currentUser'
-import { withRouter } from "react-router-dom"
+import React from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/currentUser';
+import { withRouter } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
 const Logout = ({ logout, history }) => {
 	return (
-		<form onSubmit={(event) => {
-			event.preventDefault()
-			logout(history) 
-		}
-			}>
-		<Button type="submit" variant="outline-secondary">Log Out</Button>
+		<form
+			onSubmit={event => {
+				event.preventDefault();
+				logout(history);
+			}}
+		>
+			<Button
+				type='submit'
+				variant='outline-primary'
+				style={{ border: 'none', background: 'none', fontSize: '26px' }}
+			>
+				Log Out
+			</Button>
 		</form>
+	);
+};
 
-		)
-}
-
-
-export default withRouter(connect(null, { logout })(Logout))
+export default withRouter(
+	connect(
+		null,
+		{ logout }
+	)(Logout)
+);
