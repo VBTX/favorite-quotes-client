ed37c8c67d2a39ab028c9469d23b0c8dc969c156
VBTX <jv.svedaite@gmail.com>
2019-10-08 19:47:08 -0500

    add additional text

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,34 +2,66 @@ import React from 'react';
 // import Login from './LoginForm'
 // import Signup from './SignUp'
 // import Logout from './Logout'
-import { connect } from 'react-redux'
-import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import '../../App.css';
 // import SearchQuotes from './SearchQuotes'
-import RandomQuote from "../quotes/RandomQuote"
+import RandomQuote from '../quotes/RandomQuote';
 import Button from 'react-bootstrap/Button';
 
-
-
-const Home = ({currentUser, loggedIn}) => {
-	return (<>
-
-		{ currentUser ?
-
-		<><h1 style={{"fontFamily": "'Homemade Apple', cursive"}}><b> Welcome, {currentUser.name}! </b> <RandomQuote /></h1></> :
-
-		<><h1 style={{"fontFamily": "'Homemade Apple', cursive"}}><b> Welcome, stranger! Please <Button variant="primary" size="lg" style={{"fontFamily": 'sans-serif'}}><Link to="/signup" className="button-link">SIGN UP</Link></Button> or <Button variant="success" size="lg" style={{"fontFamily": 'sans-serif'}}><Link to="/login" className="button-link">LOG IN</Link></Button>!</b></h1></>
-		
-		}
-	
-</>)}
-
+const Home = ({ currentUser, loggedIn }) => {
+	return (
+		<>
+			{currentUser ? (
+				<>
+					<h1 style={{ fontFamily: "'Homemade Apple', cursive" }}>
+						<b> Welcome, {currentUser.name}! </b> <RandomQuote />
+					</h1>
+				</>
+			) : (
+				<>
+					<h1 style={{ fontFamily: "'Homemade Apple', cursive" }}>
+						<b>
+							{' '}
+							Welcome, stranger! :)
+							<br />
+							<br />
+							<br />
+							<br />
+							If you want to start collecting your favorite quotes, please{' '}
+							<Button
+								variant='primary'
+								size='lg'
+								style={{ fontFamily: 'sans-serif' }}
+							>
+								<Link to='/signup' className='button-link'>
+									SIGN UP
+								</Link>
+							</Button>{' '}
+							or{' '}
+							<Button
+								variant='success'
+								size='lg'
+								style={{ fontFamily: 'sans-serif' }}
+							>
+								<Link to='/login' className='button-link'>
+									LOG IN
+								</Link>
+							</Button>
+							!
+						</b>
+					</h1>
+				</>
+			)}
+		</>
+	);
+};
 
 const mapStateToProps = state => {
-  return {
-    currentUser: state.currentUser,
-    loggedIn: !!state.currentUser
-  }
-}
+	return {
+		currentUser: state.currentUser,
+		loggedIn: !!state.currentUser
+	};
+};
 
-export default connect(mapStateToProps)(Home)
+export default connect(mapStateToProps)(Home);
