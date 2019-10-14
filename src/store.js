af228bc1af5ad7b8b1b41d7dd26445e761fe55bc
VBTX <jv.svedaite@gmail.com>
2019-10-14 16:26:44 -0500

    remove unnecessary import of logger

--- src/store.js
+++ src/store.js
@@ -1,14 +1,12 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux' 
-import  thunk  from 'redux-thunk'
-// import logger from "redux-logger"
-import currentUser from './reducers/currentUser'
-import loginForm from './reducers/loginForm'
-import myQuotes from './reducers/myQuotes'
-import signupForm from './reducers/signupForm'
-import quoteForm from './reducers/quoteForm'
-import search from './reducers/search'
-import categories from './reducers/categories'
-
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
+import currentUser from './reducers/currentUser';
+import loginForm from './reducers/loginForm';
+import myQuotes from './reducers/myQuotes';
+import signupForm from './reducers/signupForm';
+import quoteForm from './reducers/quoteForm';
+import search from './reducers/search';
+import categories from './reducers/categories';
 
 const reducer = combineReducers({
 	currentUser,
@@ -18,9 +16,9 @@ const reducer = combineReducers({
 	quoteForm,
 	search,
 	categories
-	})
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
-export default store
+export default store;
