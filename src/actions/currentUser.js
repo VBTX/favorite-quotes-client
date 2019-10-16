ad99c81cba068b99ad2414fcf0ee661ebfc327eb
VBTX <jv.svedaite@gmail.com>
2019-10-15 22:34:25 -0500

    delete unnecessary code

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,116 +1,105 @@
-import { resetLoginForm } from "./loginForm"
-import { getMyQuotes } from "./myQuotes"
-import { clearQuotes } from "./myQuotes"
-import { getMyCategories } from "./categories"
-import { resetSignUpForm } from "./signupForm"
-
-
+import { resetLoginForm } from './loginForm';
+import { getMyQuotes } from './myQuotes';
+import { clearQuotes } from './myQuotes';
+import { getMyCategories } from './categories';
+import { resetSignUpForm } from './signupForm';
 
 //SYNC
 
-
 export const setCurrentUser = user => {
 	return {
-		type: "SET_CURRENT_USER",
+		type: 'SET_CURRENT_USER',
 		user
-	}
-}
+	};
+};
 
-
- export const clearCurrentUser = () => {
- 	return {
- 		type: "CLEAR_CURRENT_USER"
- 	}
- }
+export const clearCurrentUser = () => {
+	return {
+		type: 'CLEAR_CURRENT_USER'
+	};
+};
 
 //ASYNC
 export const signup = (credentials, history) => {
-  return dispatch =>{
-    return fetch("http://localhost:3001/api/v1/signup",{
-      credentials: "include",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(credentials)
-    })
-    .then(r => r.json())
-    .then(user => {
-      if (user.error) {
-        alert(user.error)
-      } else {
-        dispatch(setCurrentUser(user))
-        dispatch(resetSignUpForm())
-        history.push("/")
-      }
-      })
-    .catch(console.log)
-  }
- }
-
-
-export const login = (credentials, history) => {
- 	return dispatch =>{
-		return fetch("http://localhost:3001/api/v1/login",{
-			credentials: "include",
-			method: "POST",
+	return dispatch => {
+		return fetch('http://localhost:3001/api/v1/signup', {
+			credentials: 'include',
+			method: 'POST',
 			headers: {
-				"Content-Type": "application/json"
+				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(credentials)
 		})
-		.then(r => r.json())
-		.then(user => {
-			if (user.error) {
-				alert(user.error)
-			} else {
-				dispatch(setCurrentUser(user))
-        dispatch(getMyQuotes())
-        dispatch(resetLoginForm())
-        history.push("/")
-			}
-
-
+			.then(r => r.json())
+			.then(user => {
+				if (user.error) {
+					alert(user.error);
+				} else {
+					dispatch(setCurrentUser(user));
+					dispatch(resetSignUpForm());
+					history.push('/');
+				}
 			})
-		.catch(console.log)
- 	}
-  // console.log("e")
- }
+			.catch(console.log);
+	};
+};
 
- export const getCurrentUser = () => {
-  return dispatch =>{
-    return fetch("http://localhost:3001/api/v1/get_current_user",{
-      credentials: "include",
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      },
-    })
-    .then(r => r.json())
-    .then(user => {
-      if (user.error) {
-        alert(user.error)
-      } else {
-        dispatch(setCurrentUser(user))
-        dispatch(getMyQuotes())
-        dispatch(getMyCategories())
-      }
+export const login = (credentials, history) => {
+	return dispatch => {
+		return fetch('http://localhost:3001/api/v1/login', {
+			credentials: 'include',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(credentials)
+		})
+			.then(r => r.json())
+			.then(user => {
+				if (user.error) {
+					alert(user.error);
+				} else {
+					dispatch(setCurrentUser(user));
+					dispatch(getMyQuotes());
+					dispatch(resetLoginForm());
+					history.push('/');
+				}
+			})
+			.catch(console.log);
+	};
+};
 
+export const getCurrentUser = () => {
+	return dispatch => {
+		return fetch('http://localhost:3001/api/v1/get_current_user', {
+			credentials: 'include',
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(r => r.json())
+			.then(user => {
+				if (user.error) {
+					alert(user.error);
+				} else {
+					dispatch(setCurrentUser(user));
+					dispatch(getMyQuotes());
+					dispatch(getMyCategories());
+				}
+			})
+			.catch(console.log);
+	};
+};
 
-      })
-    .catch(console.log)
-  }
- }
-
-  export const logout = (history) => {
-  return dispatch => {
-    dispatch(clearCurrentUser())
-    dispatch(clearQuotes())
-    history.push("/")
-    return fetch("http://localhost:3001/api/v1/logout", {
-      credentials: "include",
-      method: "DELETE"
-    })
-  }
- }
-
+export const logout = history => {
+	return dispatch => {
+		dispatch(clearCurrentUser());
+		dispatch(clearQuotes());
+		history.push('/');
+		return fetch('http://localhost:3001/api/v1/logout', {
+			credentials: 'include',
+			method: 'DELETE'
+		});
+	};
+};
