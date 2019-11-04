957f8debd77cebafdbdf0d00344980006e6af6b7
VBTX <jv.svedaite@gmail.com>
2019-11-04 17:13:25 -0600

    make myQuotes list responsive

--- src/components/quotes/MyQuotes.js
+++ src/components/quotes/MyQuotes.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import QuickQuoteCard from './QuickQuoteCard';
+import './MyQuotes.css';
 
 const MyQuotes = props => {
 	const quoteCards =
 		props.quotes.length > 0 ? (
-			<div className='myQuotes'>
-				<br />
-				{props.quotes.map(q => (
-					<QuickQuoteCard key={q.id} quote={q} />
-				))}
+			<div className='container'>
+				<div className='myQuotes'>
+					<br />
+					{props.quotes.map(q => (
+						<QuickQuoteCard key={q.id} quote={q} />
+					))}
+				</div>
 			</div>
 		) : (
 			'No quotes at the moment'
