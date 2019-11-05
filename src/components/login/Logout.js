f52daa04f3472703c0ca90da109d59907a936e18
VBTX <jv.svedaite@gmail.com>
2019-11-05 11:35:33 -0600

    made a NavLink out of the button in order to make NavBar stylistically consistent

--- src/components/login/Logout.js
+++ src/components/login/Logout.js
@@ -2,25 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { logout } from '../../actions/currentUser';
 import { withRouter } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+import { NavLink } from 'react-router-dom';
+// import Button from 'react-bootstrap/Button';
 
 const Logout = ({ logout, history }) => {
 	return (
-		<form
-			onSubmit={event => {
-				event.preventDefault();
-				logout(history);
-			}}
+		<NavLink
+			exact
+			type='submit'
+			onClick={() => logout(history)}
+			className='nav-link'
+			to='/'
 		>
-			<Button
-				type='submit'
-				className='nav-link'
-				variant='outline-primary'
-				style={{ border: 'none', background: 'none', fontSize: '28px' }}
-			>
-				Log Out
-			</Button>
-		</form>
+			Log Out
+		</NavLink>
 	);
 };
 
