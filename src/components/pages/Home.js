2b403607c08330f3be281f5392d23b3954739e14
VBTX <jv.svedaite@gmail.com>
2020-01-13 22:45:30 -0600

    remove unnecessary breaks

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,60 +1,110 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import '../../App.css';
-import RandomQuote from '../quotes/RandomQuote';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import "../../App.css";
+import RandomQuote from "../quotes/RandomQuote";
+import Carousel from "react-bootstrap/Carousel";
+import "../quotes/QuickCard.css";
 
-const Home = ({ currentUser, loggedIn }) => {
-	return (
-		<>
-			{currentUser ? (
-				<>
-					<br />
-					<br />
-					<br />
-					<br />
-					<h2 style={{ fontFamily: "'Special Elite', cursive" }}>
-						<b> Welcome, {currentUser.name}! </b> <RandomQuote />
-					</h2>
-				</>
-			) : (
-				<div className='container'>
-					<h1 style={{ fontFamily: "'Special Elite', cursive" }}>
-						<b>
-							{' '}
-							Welcome, stranger! :)
-							<br />
-							<br />
-							If you want to start collecting your favorite quotes,
-							<br /> please{' '}
-							<Link
-								to='/signup'
-								className='button-link'
-								style={{ background: 'black', padding: '1%' }}
-							>
-								SIGN UP
-							</Link>
-							or{' '}
-							<Link
-								to='/login'
-								className='button-link'
-								style={{ background: 'black', padding: '1%' }}
-							>
-								LOG IN
-							</Link>
-						</b>
-					</h1>
-				</div>
-			)}
-		</>
-	);
-};
+class Home extends React.Component {
+  constructor() {
+    super();
 
-const mapStateToProps = state => {
-	return {
-		currentUser: state.currentUser,
-		loggedIn: !!state.currentUser
-	};
+    this.state = {
+      quotes: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/v1/quotes")
+      .then((response) => response.json())
+      .then((quotes) => this.setState({ quotes }));
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        {this.props.currentUser ? (
+          <>
+            <br />
+
+            <h2
+              style={{
+                fontFamily: "'Special Elite', cursive"
+              }}
+            >
+              <b> Welcome, {this.props.currentUser.name}! </b> <RandomQuote />
+            </h2>
+          </>
+        ) : (
+          <>
+            <h3
+              style={{ fontFamily: "'Special Elite', cursive", padding: "2%" }}
+            >
+              <b>
+                {" "}
+                Welcome, stranger! :)
+                <br />
+                <br />
+                This app allows you to collect your favorite quotes and related
+                notes. If you want to start your personal quote collection,
+                <br /> please{" "}
+                <Link
+                  to='/signup'
+                  className='button-link'
+                  style={{ background: "black", padding: "1%" }}
+                >
+                  SIGN UP
+                </Link>
+                or{" "}
+                <Link
+                  to='/login'
+                  className='button-link'
+                  style={{ background: "black", padding: "1%" }}
+                >
+                  LOG IN
+                </Link>
+              </b>
+            </h3>
+            <div style={{ justifyContent: "center" }}>
+              <Carousel fade={true}>
+                {this.state.quotes.map((quote) => (
+                  <Carousel.Item className='carousel-body'>
+                    <hr />
+                    <h3>
+                      <i>
+                        <strong>{quote.text || quote.quote}</strong>
+                      </i>
+                    </h3>
+
+                    <h5>
+                      Author:{" "}
+                      <a
+                        href={`https://www.google.com/search?q=${quote.author}`}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                      >
+                        {quote.author}
+                      </a>
+                      ; Source:{" "}
+                      {quote.source || <a href='theysaidso.com'>TheySaidSo</a>}
+                    </h5>
+                  </Carousel.Item>
+                ))}
+              </Carousel>
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.currentUser,
+    loggedIn: !!state.currentUser
+  };
 };
 
 export default connect(mapStateToProps)(Home);
